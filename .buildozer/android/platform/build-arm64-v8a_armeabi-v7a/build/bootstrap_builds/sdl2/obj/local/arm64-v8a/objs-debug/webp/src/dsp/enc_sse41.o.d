c1d94a330aaacde2bc4f581ad22833644deba8e6
Michal Malinowski <michumalinowski@gmail.com>
2023-08-09 23:35:03 +0200

    previous version didn't open on android

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/dsp/enc_sse41.o.d
+++
@@ -1,8 +0,0 @@
-/home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/dsp/enc_sse41.o: \
-  /home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/enc_sse41.c \
-  /home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
-  /home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
-
-/home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/vm-ubuntu/Documents/GrzybApka/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/vm-ubuntu/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.c \
-  /home/vm-ubuntu/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h
-
-/home/vm-ubuntu/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h:
